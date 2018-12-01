e45acd3b94a7a96a983e5fbdeaf19f1c94b18b67
Agnes Ribeiro <mu21@hotmail.fr>
2018-12-01 16:13:55 +0100

    Updates

---
+++ precache-manifest.0f4b5a5f1f2c9abaf2029651b8db6698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae77bb3d2c7b4690933e",
+    "url": "/project-perso-1/static/js/runtime~main.ae77bb3d.js"
+  },
+  {
+    "revision": "245986bd5ec7140e765b",
+    "url": "/project-perso-1/static/js/main.245986bd.chunk.js"
+  },
+  {
+    "revision": "d63210a8a06c94e20f30",
+    "url": "/project-perso-1/static/js/1.d63210a8.chunk.js"
+  },
+  {
+    "revision": "245986bd5ec7140e765b",
+    "url": "/project-perso-1/static/css/main.a0931ff2.chunk.css"
+  },
+  {
+    "revision": "d63210a8a06c94e20f30",
+    "url": "/project-perso-1/static/css/1.69c81e2e.chunk.css"
+  },
+  {
+    "revision": "a9df48add08529f86f53127348b44c4a",
+    "url": "/project-perso-1/index.html"
+  }
+];
